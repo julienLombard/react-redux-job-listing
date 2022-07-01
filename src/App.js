210d30dc4814fba336651f4a3343747385b47ae4
julienLombard <julienlombard.fr@gmail.com>
2022-07-01 12:36:46 +0200

    React Redux installation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="App-main">
+        <h1>Job Listing</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
